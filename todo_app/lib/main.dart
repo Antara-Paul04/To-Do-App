5cd6d64ee37e3f000fb473e7d8af62cd580f9806
Antara Paul <hiiamantara04@gmail.com>
2024-03-23 21:24:55 +0530

    Added:functionalities to mark and unmark tasks and to delete tasks

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -6,8 +6,25 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   final todosList = ToDo.todoList();
+
+  void handleTodoChange(ToDo todo) {
+    setState(() {
+      todo.isDone = !todo.isDone;
+    });
+  }
+
+  void deleteItem(String id) {
+    setState(() {
+      todosList.removeWhere((item) => item.id == id);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +60,11 @@ class MyApp extends StatelessWidget {
                         for (ToDo todo in todosList)
                           Column(
                             children: [
-                              Tasks(todo: todo),
+                              Tasks(
+                                todo: todo,
+                                onTodoChanged: handleTodoChange,
+                                onDeleteItem: deleteItem,
+                              ),
                               SizedBox(height: 20),
                             ],
                           ),
@@ -65,15 +86,19 @@ class MyApp extends StatelessWidget {
                           decoration: InputDecoration(
                             border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(50),
-                              borderSide: BorderSide(color: Color(0xFF745E96)),
+                              borderSide: BorderSide(
+                                color: Color(0xFF745E96),
+                              ),
                             ),
                             hintText: "Add new task",
-                            contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 20,
+                              horizontal: 20,
+                            ),
                           ),
                         ),
                       ),
-                      SizedBox(width: 15,),
+                      SizedBox(width: 15),
                       Container(
                         height: 70,
                         width: 70,
@@ -107,7 +132,7 @@ Widget searchBox() {
     padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
     child: TextField(
       decoration: InputDecoration(
-      contentPadding: EdgeInsets.all(20),
+        contentPadding: EdgeInsets.all(20),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(100),
           borderSide: BorderSide(color: Color(0xFF745E96)),
--- todo_app/lib/widgets/tasks.dart
+++ todo_app/lib/widgets/tasks.dart
@@ -3,39 +3,53 @@ import '../models/todo.dart';
 
 class Tasks extends StatelessWidget {
   final ToDo todo;
+  final Function(ToDo) onTodoChanged;
+  final Function(String) onDeleteItem;
 
-  const Tasks({Key? key, required this.todo}): super(key:key);
+  const Tasks({
+    Key? key,
+    required this.todo,
+    required this.onTodoChanged,
+    required this.onDeleteItem,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: ListTile(
-        contentPadding: EdgeInsets.fromLTRB(20,5,10,5),
+        onTap: () {
+          onTodoChanged(todo);
+        },
+        contentPadding: EdgeInsets.fromLTRB(20, 5, 10, 5),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        tileColor: Color(0xFFD1C1F2),
-        leading: todo.isDone? Icon(
-          Icons.check_box, 
-          color:Color.fromARGB(255, 99, 62, 154),
-          ): Icon(
-            Icons.check_box_outline_blank, 
-            color: Color.fromARGB(255, 99, 62, 154)),
+        tileColor: todo.isDone ? Color.fromARGB(255, 173, 149, 221) : Color(0xFFD1C1F2),
+        leading: todo.isDone
+            ? Icon(
+                Icons.check_box,
+                color: Color.fromARGB(255, 99, 62, 154),
+              )
+            : Icon(
+                Icons.check_box_outline_blank,
+                color: Color.fromARGB(255, 99, 62, 154),
+              ),
         title: Row(
           children: [
             Text(
-              todo.todoText!, 
+              todo.todoText!,
               style: TextStyle(
                 color: Colors.black,
                 fontSize: 18,
-                decoration: todo.isDone? TextDecoration.lineThrough: null,
+                decoration: todo.isDone ? TextDecoration.lineThrough : null,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(180, 8,0,8),
-              child: IconButton(
-                onPressed: (){
-                  print("Delete button was pressed");
-                },
-                icon: Icon(Icons.delete),
+            Spacer(),
+            GestureDetector(
+              onTap: () {
+                onDeleteItem(todo.id.toString());
+              },
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Icon(Icons.delete),
               ),
             ),
           ],
